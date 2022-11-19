72b311734d76533f739d8fb5e8b6953baa65da7e
Aarjav Shah <classypark@MacBook-Pro-7.local>
2022-11-19 21:41:52 +0100

    problem detail implemntation added

--- src/main/java/com/cgi/bni/spring3demo/controller/EmployeeExceptionHandler.java
+++ src/main/java/com/cgi/bni/spring3demo/controller/EmployeeExceptionHandler.java
@@ -2,16 +2,27 @@ package com.cgi.bni.spring3demo.controller;
 
 import com.cgi.bni.spring3demo.exception.EmployeeNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.net.URI;
+
+@RestControllerAdvice
 public class EmployeeExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void handleNotFound() {
-
+    public ProblemDetail handleNotFound(EmployeeNotFoundException exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                exception.getMessage());
+        problemDetail.setProperty("EmployeeId", exception.getEmployeeId());
+        problemDetail.setType(URI.create("https://confluence"));
+        return problemDetail;
     }
+
+//    @ExceptionHandler(EmployeeNotFoundException.class)
+//    public ProblemDetail handleNotFound(EmployeeNotFoundException exception) {
+//        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+//                exception.getMessage());
+//    }
 }
